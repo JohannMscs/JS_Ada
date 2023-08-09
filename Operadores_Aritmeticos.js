b3d9012d3f11bd3e16e018b17bb2f25aeba70666
Johann Moscoso <110245986+JohannMscs@users.noreply.github.com>
2023-08-09 11:05:08 -0300

    Add files via upload

---
+++ Operadores_Aritmeticos.js
@@ -0,0 +1,29 @@
+let numero1 = 25;
+let numero2 = 14;
+
+//adicao +
+//subtracao -
+// multiplicacao *
+//divisao /
+//exponeciacao **
+// resto da divisao %
+
+let soma = numero1 + numero2
+console.log(`A soma = ${soma}`);
+console.log("A subtracao = ", numero1 - numero2);
+console.log("A multiplicacao = ", numero1 * numero2);
+console.log("A divisao = ", numero1 / numero2);
+console.log("A exponeciacao = ", numero1 ** numero2);
+console.log("O resto da divisao = ", numero1 % numero2);
+
+// precedencia de operadores
+
+console.log(3+5*4-5)
+console.log(75/5+5*50)
+console.log((4+10+5) /3) 
+
+//biblioteca Math
+
+console.log(Math.PI)
+console.log(Math.sqrt(100))
+
