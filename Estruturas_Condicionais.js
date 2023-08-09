b5632408d3823c94e7c6a93509ac4bbcb9f3e975
Johann Moscoso <110245986+JohannMscs@users.noreply.github.com>
2023-08-09 19:18:11 -0300

    Add files via upload

---
+++ Estruturas_Condicionais.js
@@ -0,0 +1,24 @@
+let idade = 8
+if(idade >=18){
+    console.log("você é maior de idade")
+}
+else{
+    console.log("você é menor de idade")
+}
+console.clear()
+
+// se media for >=7, aprovado
+// se a e < 7 e media >=5, recuperacao
+// se media <5, reprovado
+
+const nota = 4
+let media = 7
+if(nota >= media){
+    console.log("você foi aprovado")
+}
+else if(nota < media && nota >=5){
+    console.log("você vai para recuperação")
+}
+else{
+    console.log("você foi reprovado")
+}
