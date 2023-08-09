bea556d45eeadd5124babaff85e369eb4a05be71
Johann Moscoso <110245986+JohannMscs@users.noreply.github.com>
2023-08-09 19:39:37 -0300

    Add files via upload

---
+++ Estruturas_de_Repeticao.js
@@ -0,0 +1,14 @@
+//laços condicionais
+
+const input = require('readline-sync')
+
+const numero_sorteado = 5;
+
+let numero = Number (input.question("Qual numero voce escolhe ? "))
+
+while(numero !== numero_sorteado){
+    console.log("voce errou o numero, tente novamente...")
+
+    numero = Number(input.question("Qual numero voce escolhe ?"))
+}
+ console.log("voce acertou!")
