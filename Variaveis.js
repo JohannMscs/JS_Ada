cb98114938c41ce08bf9328b8d430ec271affbda
Johann Moscoso <110245986+JohannMscs@users.noreply.github.com>
2023-08-09 10:41:13 -0300

    Add files via upload

---
+++ Variaveis.js
@@ -0,0 +1,28 @@
+//Variaveis
+
+let n1 = 21;
+let n2 = 34;
+let n3 = n1+n2;
+console.log(n3);
+
+let idade = 21
+console.log(`A sua idade é ${idade}`)
+
+// tipos de variaveis
+
+let peso = 60; //number
+let altura = 1.68;  //number
+let nome = "Robson"; //string
+let estudando = true //boolean
+let outra;
+const constante = 100;
+//constante = 55;
+var variavel = 7; //nao recomendado
+variavel=50;   
+
+console.log(peso, typeof peso)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+console.log(outra, typeof outra)
+console.log(variavel)
---
+++ javascript.js
@@ -0,0 +1,3 @@
+console.log("Hello, world")
+console.log("Bem vindo ao curso de Javascript!")
+//estou imprimindo duas mensagens no terminal
